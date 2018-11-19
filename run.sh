07249e49755b40376c7e73a22c4bf06ef60e0204
Unknown <sh.mn.nat@gmail.com>
2018-11-19 09:25:48 +0900

    単回帰分析はできた

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-DIR=$(dirname $1)   # 親ディレクトリ名を取得
-FILE=$(basename $1)
+# DIR=$(dirname $1)   # 親ディレクトリ名を取得
+# FILE=$(basename $1)
+# CMD="python ${FILE}"
+
+NOTE_DIR=${PWD}/notebooks
 DATA_DIR=/data2/naoya/sexy_datascience
-CMD="python ${FILE}"
 
 docker run -it --rm \
     -v ${PWD}:/workdir \
+    -v ${NOTE_DIR}:/opt/notebooks \
     -v ${DATA_DIR}:/data \
     -w /workdir/${DIR} \
     -p 8888:8888 \
-    denden047/datascience \
-    /bin/bash -c "${CMD}"
+    denden047/datascience:jupyter
+# multiple_regression_analysis.ipynb
