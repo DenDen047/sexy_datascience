d16fadd012f5f0a01188e55c38386341adf87b57
DenDen047 <sh.mn.nat@gmail.com>
2018-12-12 16:53:02 +0900

    jupyterが動くようになった

--- run.sh
+++ run.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# DIR=$(dirname $1)   # 親ディレクトリ名を取得
-# FILE=$(basename $1)
-# CMD="python ${FILE}"
-
 NOTE_DIR=${PWD}/notebooks
 DATA_DIR=/data2/naoya/sexy_datascience
 
 docker run -it --rm \
     -v ${NOTE_DIR}:/notebooks \
     -v ${DATA_DIR}:/data \
-    -v ${PWD}/configs/.jupyter:/root/.jupyter \
-    -w /workdir/${DIR} \
+    -v ${PWD}/configs/.jupyter:/root/.jupyter:ro \
     -p 8888:8888 \
-    denden047/datascience:jupyter
+    denden047/datascience:jupyter \
+    /bin/bash -c "jupyter notebook --port=8888 --allow-root"
