f5381971e820f54fa6dbe8226e8c6bd1a4ec9397
DenDen047 <sh.mn.nat@gmail.com>
2018-12-13 09:25:36 +0900

    update

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ DATA_DIR=/data2/naoya/sexy_datascience
 docker run -it --rm \
     -v ${NOTE_DIR}:/notebooks \
     -v ${DATA_DIR}:/data \
-    -v ${PWD}/configs/.jupyter:/root/.jupyter:ro \
+    -v ${PWD}/configs/.jupyter:/root/.jupyter \
     -p 8888:8888 \
     denden047/datascience:jupyter \
     /bin/bash -c "jupyter notebook --port=8888 --allow-root"
